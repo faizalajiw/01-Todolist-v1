22effee776d9dcee59868c31b695886f02d625cd
faizalajiw <faizalajiwibowo7@gmail.com>
2022-03-05 09:57:29 +0700

    bug fixes

--- main.js
+++ main.js
@@ -1,61 +1,88 @@
-const addBtn = document.querySelector('.addBtn');
-var input = document.querySelector('.input');
 const container = document.querySelector('.container');
+var inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
 
-class item {
-  constructor(itemName) {
-    // membuat item
-    this.createDiv(itemName);
-  }
-
-  createDiv(itemName) {
-    let input = document.createElement('input');
-    input.value = itemName;
-    input.disabled = true;
-    input.classList.add('item_input');
-    input.type = 'text';
-
-    let itemBox = document.createElement('input');
-    itemBox.classList.add('item');
-
-    let editBtn = document.createElement('button');
-    editBtn.innerHTML = 'Edit';
-    editBtn.classList.add('editBtn');
-
-    let removeBtn = document.createElement('button');
-    removeBtn.innerHTML = 'Remove';
-    removeBtn.classList.add('removeBtn');
-
-    container.appendChild(itemBox);
-
-    itemBox.appendChild(input);
-    itemBox.appendChild(editBtn);
-    itemBox.appendChild(removeBtn);
-
-    editBtn.addEventListener('click', () => this.edit(input));
-
-    removeBtn.addEventListener('click', () => this.edit(itemBox));
-  }
-
-  edit(input) {
-    input.disabled = !input.disabled;
-  }
-
-  remove(item) {
-    container.removeChild(item);
-  }
+if(window.localStorage.getItem("todos") == undefined){
+     var todos = [];
+     window.localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function check() {
-  if (input.value != "") {
-    new item(input.value);
-    input.value = "";
-  }
+var todosEX = window.localStorage.getItem("todos");
+var todos = JSON.parse(todosEX);
+
+
+class item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	var itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+    	var input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item_input');
+
+    	var edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = '<i class="las la-edit"></i>';
+    	edit.addEventListener('click', () => this.edit(input, name));
+
+    	var remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = '<i class="las la-trash"></i>';
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
+    }
+
+    edit(input, name){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
+        }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = todos.indexOf(name);
+            todos[indexof] = input.value;
+            window.localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }
+
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
 }
 
-addBtn.addEventListener('click', check);
+add.addEventListener('click', check);
 window.addEventListener('keydown', (e) => {
-  if (event.keyCode === 13) {
-    check();
-  }
+	if(e.which == 13){
+		check();
+	}
 })
+
+function check(){
+	if(inputValue.value != ""){
+		new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+		inputValue.value = "";
+	}
+}
+
+
+for (var v = 0 ; v < todos.length ; v++){
+    new item(todos[v]);
+}
+
+
+new item("sport");
